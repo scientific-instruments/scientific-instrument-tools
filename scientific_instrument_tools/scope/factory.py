f17817fa318c1bc62532a95404412a7421a268b1
Gaël Pongnot <35938145+gpongnot@users.noreply.github.com>
2025-06-03 17:12:45 +0200

    Sorting the scope folder for future developments

--- scientific_instrument_tools/scope/factory.py
+++ scientific_instrument_tools/scope/factory.py
@@ -14,6 +14,10 @@ class OscilloscopeFactory:
         cls.__unknown_message__(resource)
 
     @classmethod
+    def make_rohdeschwarz(cls, resource: ResourceItem):
+        cls.__unknown_message__(resource)
+
+    @classmethod
     def __unknown_message__(cls, resource: ResourceItem):
         raise NotImplementedError(f"Unknown model {resource.model} for {resource.manufacturer}\n"
          f"The requested resource may exist but is not implemented yet. SIT is a collaborative library, feel free to contribute with an implementation for your hardware!\n")
--- scientific_instrument_tools/scope/scope_rhodeschwarz.py
+++ scientific_instrument_tools/scope/scope_rhodeschwarz.py
@@ -3,8 +3,8 @@
 from ..core import ResourceItem
 from .scope import Oscilloscope
 
-class OscilloscopeTektronix(Oscilloscope):
-    """Generic class for Tektronix Oscilloscopes."""
+class OscilloscopeRhodeSchwartz(Oscilloscope):
+    """Generic class for Rhode & Schwartz Oscilloscopes."""
 
     def __init__(self, resource: ResourceItem):
         super().__init__(resource)
